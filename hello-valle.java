7db27eeea9acab3c232e968cfb4f9d58af5873ca
Valle1616 <antonio.vallefuoco@capellinisauro.edu.it>
2025-05-12 07:20:53 +0000

    Aggiunta stampa da Vector

--- hello-valle.java
+++ hello-valle.java
@@ -3,5 +3,21 @@ public class HelloValle{
     public static void main(String[] args)
     {
         System.out.println("hello TOTO");
-    }
+    
+        Vector<Character> lettere= new  Vector<>();
+        lettere.add('a');
+        l lettere.add('n');
+        lettere.add('t');
+         lettere.add('o');
+         lettere.add('n');
+         lettere.add('i');
+         lettere.add('o ');
+        
+        for(Character lettera: lettere){
+            System.out.println(lettera);
+        }
+        
+     
+
+}
 }
